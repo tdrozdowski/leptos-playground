ba6828a1647d7bbb8269b901496f18268b21956f
terry <terry@xymox.dev>
2024-01-01 00:56:31 -0700

    how to handle input validation errors

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
 use leptos::*;
-use leptos::ev::SubmitEvent;
-use leptos::html::Input;
 
 fn main() {
     leptos::mount_to_body(|| view! {<App/>})
 }
 
-
 #[component]
 fn App() -> impl IntoView {
-    let (name, set_name) = create_signal("Uncontrolled".to_string());
-    let input_element: NodeRef<Input> = create_node_ref();
-
-    let on_submit = move |ev: SubmitEvent| {
-        ev.prevent_default();
-        let value = input_element.get()
-            .expect("<input> to exist")
-            .value();
-        set_name.set(value);
-    };
+    let (value, set_value) = create_signal(Ok(0));
+    let on_input = move |ev| set_value.set(event_target_value(&ev).parse::<i32>());
     view! {
-        <form on:submit=on_submit>
-            <input type="text"
-                value=name
-                node_ref=input_element
-            />
-            <input type="submit" value="Submit"/>
-        </form>
-        <p>"Name is: "{name}</p>
+        <h1>"Error Handling"</h1>
+        <label>"Type a number (or something that is not a number!)"
+            <input type="number" on:input=on_input/>
+            <ErrorBoundary
+                fallback=|errors| view! {
+                    <div class="error">
+                        <p>"Not a number! Errors:"</p>
+                        <ul>
+                            { move || errors.get()
+                                        .into_iter()
+                                        .map(|(_, e)| view! {<li>{e.to_string()}</li>})
+                                        .collect::<Vec<_>>()
+                            }
+                        </ul>
+                    </div>
+                }
+            >
+                <p>"You entered: "<strong>{value}</strong></p>
+            </ErrorBoundary>
+        </label>
     }
-
-
 }
