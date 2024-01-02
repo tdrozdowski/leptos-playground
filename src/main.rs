d45c2ccdcd94a66cb1d187896fec821e3a30b313
terry <terry@xymox.dev>
2024-01-01 23:12:04 -0700

    working with transitions

--- src/main.rs
+++ src/main.rs
@@ -5,49 +5,51 @@ fn main() {
     leptos::mount_to_body(|| view! {<App/>})
 }
 
-async fn load_data(value: i32) -> i32 {
+async fn important_api_call(id: usize) -> String {
     TimeoutFuture::new(1_000).await;
-    value * 10
+    match id {
+        0 => "Alice",
+        1 => "Bob",
+        2 => "Carol",
+        _ => "User Not Found",
+    }.to_string()
 }
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let async_data = create_resource(move || count.get(), |value| async move { load_data(value).await });
-    let stable = create_resource(|| (), |_| async move { load_data(1).await });
-    let async_result = move || {
-        async_data
-            .get()
-            .map(|value| format!("Server returned {value:?}"))
-            .unwrap_or_else(|| "Loading....".into())
-    };
-    let loading = async_data.loading();
-    let is_loading = move || {
-        if loading.get() {
-            "Loading..."
-        } else {
-            "Idle"
-        }
-    };
+    let (tab, set_tab) = create_signal(0);
+    let user_data = create_resource(move || tab.get(),  |tab| async move  { important_api_call(tab).await});
     view! {
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
+        <div class="buttons">
+            <button
+                on:click=move |_| set_tab.set(0)
+                class:selected=move || tab.get() == 0>
+                "Tab A"
+            </button>
+            <button
+                on:click=move |_| set_tab.set(1)
+                class:selected=move || tab.get() == 1>
+                "Tab B"
+            </button>
+            <button
+                on:click=move |_| set_tab.set(2)
+                class:selected=move || tab.get() == 2>
+                "Tab C"
+            </button>
+            {move || if user_data.loading().get() {
+                        "Loading..."
+                    } else {
+                        ""
+                    }
             }
+        </div>
+        <Transition
+            fallback=move || view! {<p>"Loading..."</p>}
         >
-            "Click Me"
-        </button>
-        <p>
-            <code>Stable:</code>": " {move || stable.get()}
-        </p>
-        <p>
-            <code>Count:</code>": " {count}
-        </p>
-        <p>
-            <code>async_value</code>": "
-            {async_result}
-            <br/>
-            {is_loading}
-        </p>
+            <p>
+                {move || user_data.get()}
+            </p>
+        </Transition>
     }
+
 }
